0970c7f42815177d4ec1ffa0d84bb2e5dd9ca32a
Tammy Lam <lamtammy1515@gmail.com>
2020-03-25 21:24:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,4 +86,9 @@ end
     sql = "SELECT * FROM students WHERE grade = ?"
     DB[:conn].execute(sql, x)
   end
+  
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql)
+  end
 end
