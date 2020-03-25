76a28cf5f21c392cffd7f221f37dd0f1acc2f8be
Tammy Lam <lamtammy1515@gmail.com>
2020-03-25 21:44:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
   end
 end 
 
-  def self.first_X_students_in_grade_10(grade)
+  def self.first_X_students_in_grade_10(x)
          sql = <<-SQL
        SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
