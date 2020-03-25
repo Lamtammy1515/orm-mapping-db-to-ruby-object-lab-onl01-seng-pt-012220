287b5c12271c07625ae8ba48a3e409e31fe06b21
Tammy Lam <lamtammy1515@gmail.com>
2020-03-25 21:35:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,8 +80,9 @@ end
   
    def self.first_student_in_grade_10
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
-    first_student_row = DB[:conn].execute(sql)[0]
-    self.new_from_db(first_student_row)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
    def self.all_students_in_grade_X(x)
