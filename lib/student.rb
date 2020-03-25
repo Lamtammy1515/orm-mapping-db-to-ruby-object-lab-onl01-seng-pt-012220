74dc02337e63b28e2f7339ac2dff3766991dd471
Tammy Lam <lamtammy1515@gmail.com>
2020-03-25 21:26:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,5 +94,8 @@ end
   
   def self.all_students_in_grade_X(x)
      sql = "SELECT * FROM students WHERE grade = ?"
+     DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end 
 end
